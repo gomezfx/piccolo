4932c6a88bd53252c3d8d24975dea8db474b810b
Aldo Gomez <gomezfx@gmail.com>
2019-12-28 21:51:49 -0800

    added electron

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import splash from './piccolo.png';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      backgroundStyle: {
+          'background': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+      }
+    }
+  }
+
+  onClick = (e) => {
+    this.setState({
+      backgroundStyle: {
+        'background': '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" style={this.state.backgroundStyle} onClick={this.onClick}>
+        <img className="noselect" src={splash} height="300"/>
+      </div>
+    );
+  }
+};
 
 export default App;
